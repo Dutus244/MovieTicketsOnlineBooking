0d51c8a31f1ea5bd8fbc07b8e51d92670740db08
Samuel <20127610@student.hcmus.edu.vn>
2023-03-31 18:11:37 +0700

    Revert "Admin main menu UI and Cinema UI"
    This reverts commit 61f18ebcadbbfa1e0fe86c82d0727234e420763c.

--- Admin/app/src/main/java/com/example/admin/RequestCode.kt
+++
@@ -1,8 +0,0 @@
-package com.example.admin
-
-object RequestCode {
-    const val CINEMA_SCREEN_ADD = 100
-    const val CINEMA_SCREEN_EDIT = 101
-    const val MOVIE_SCREEN_ADD = 200
-    const val MOVIE_SCREEN_EDIT = 201
-}
