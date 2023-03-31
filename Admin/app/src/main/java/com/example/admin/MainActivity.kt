61f18ebcadbbfa1e0fe86c82d0727234e420763c
Samuel <20127610@student.hcmus.edu.vn>
2023-03-31 18:04:06 +0700

    Admin main menu UI and Cinema UI

--- Admin/app/src/main/java/com/example/admin/MainActivity.kt
+++ Admin/app/src/main/java/com/example/admin/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.example.admin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.example.admin.cinemas.AddCinema
 
 class MainActivity : AppCompatActivity() {
+    var goToCinemaBtn: Button? = null
+    var goToMovieBtn: Button? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        goToCinemaBtn = findViewById(R.id.goToCinemaBtn)
+        goToMovieBtn = findViewById(R.id.goToMovieBtn)
+
+        goToCinemaBtn!!.setOnClickListener {
+            val intent = Intent(this, AddCinema::class.java)
+            startActivity(intent)
+        }
+        goToMovieBtn!!.setOnClickListener {
+
+        }
     }
 }
