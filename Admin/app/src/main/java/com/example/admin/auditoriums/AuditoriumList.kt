d8928f7cb251aa31cf913a896cd0bfd1a5c0838b
Samuel <20127610@student.hcmus.edu.vn>
2023-04-01 22:45:41 +0700

    add SeatBookView library

---
+++ Admin/app/src/main/java/com/example/admin/auditoriums/AuditoriumList.kt
@@ -0,0 +1,20 @@
+package com.example.admin.auditoriums
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.admin.R
+import dev.jahidhasanco.seatbookview.SeatBookView
+
+class AuditoriumList : AppCompatActivity() {
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_auditorium_list)
+
+
+
+
+    }
+}
--- Admin/app/src/main/java/com/example/admin/cinemas/EditCinema.kt
+++ Admin/app/src/main/java/com/example/admin/cinemas/EditCinema.kt
@@ -1,5 +1,6 @@
 package com.example.admin.cinemas
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -7,6 +8,7 @@ import android.widget.RadioButton
 import android.widget.RadioGroup
 import android.widget.TextView
 import com.example.admin.R
+import com.example.admin.auditoriums.AuditoriumList
 
 class EditCinema : AppCompatActivity() {
     var cinemaNameTV: TextView? = null
@@ -15,6 +17,7 @@ class EditCinema : AppCompatActivity() {
     var cinemaAudiNumTV: TextView? = null
     var statusRadioGroup: RadioGroup? = null
     var statusRadioButton: RadioButton? = null
+    var audiBtn: Button? = null
     var delBtn: Button? = null
     var saveBtn: Button? = null
 
@@ -27,9 +30,14 @@ class EditCinema : AppCompatActivity() {
         cinemaAddrTV = findViewById(R.id.editCinemaAddrTV)
         cinemaAudiNumTV = findViewById(R.id.editCinemaAudiNumTV)
         statusRadioGroup = findViewById(R.id.editCinemaStatusRG)
+        audiBtn = findViewById(R.id.editCinemaAudiBtn)
         delBtn = findViewById(R.id.editCinemaDeleteBtn)
         saveBtn = findViewById(R.id.editCinemaSaveBtn)
 
+        audiBtn!!.setOnClickListener {
+            val intent = Intent(this, AuditoriumList::class.java)
+            startActivity(intent)
+        }
         delBtn!!.setOnClickListener {
             finish()
         }
