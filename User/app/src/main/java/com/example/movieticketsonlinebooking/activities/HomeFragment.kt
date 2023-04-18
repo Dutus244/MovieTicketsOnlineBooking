ab98accfb0101b8ed11d6b975e2f4b0f15f490c3
Samuel <20127610@student.hcmus.edu.vn>
2023-04-18 21:08:28 +0700

    get data but still not make UI wait for data

--- User/app/src/main/java/com/example/movieticketsonlinebooking/activities/HomeFragment.kt
+++ User/app/src/main/java/com/example/movieticketsonlinebooking/activities/HomeFragment.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.graphics.Color
 import com.example.movieticketsonlinebooking.R
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -15,7 +16,12 @@ import com.example.movieticketsonlinebooking.fragments.Item
 import com.example.movieticketsonlinebooking.fragments.MyGridAdapter
 import com.example.movieticketsonlinebooking.fragments.The_Slide_Items_Model_Class_HomePage
 import com.example.movieticketsonlinebooking.fragments.The_Slide_items_Pager_Adapter_HomePage
+import com.example.movieticketsonlinebooking.viewmodels.Movie
 import com.google.android.material.tabs.TabLayout
+import com.google.firebase.FirebaseApp
+import com.google.firebase.firestore.Query
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import java.util.*
 import kotlin.collections.ArrayList
 
@@ -23,13 +29,16 @@ class HomeFragment : Fragment() {
     private var listItems: ArrayList<The_Slide_Items_Model_Class_HomePage>? = ArrayList()
     private var page: ViewPager? = null
     private var tabLayout: TabLayout? = null
-    var adapter : MyGridAdapter? = null
+    var adapter: MyGridAdapter? = null
     var gridView: GridView? = null
     var buttonCurrentFilm: Button? = null
     var buttonComingFilm: Button? = null
     var current: Int? = 1
     var arrayList: ArrayList<Item> = ArrayList()
     var buttonMore: Button? = null
+
+    var sliderMovies: List<Movie> = listOf()
+    var airingMovies: List<Movie> = listOf()
 
     inner class The_slide_timer : TimerTask() {
         override fun run() {
@@ -48,34 +57,64 @@ class HomeFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        FirebaseApp.initializeApp(requireContext())
     }
 
-    fun getDataForSlider () {
+    fun getDataForSlider() {
         // Lấy 3 item mới nhất
-        listItems!!.add(The_Slide_Items_Model_Class_HomePage(R.drawable.foreplay_background, "Bệnh viện Hoàn Mỹ"))
-        listItems!!.add(The_Slide_Items_Model_Class_HomePage(R.drawable.foreplay_background, "Bệnh viện Hoàn Mỹ"))
-        listItems!!.add(The_Slide_Items_Model_Class_HomePage(R.drawable.foreplay_background, "Bệnh viện Hoàn Mỹ"))
+        val db = Firebase.firestore
+        db.collection("movie")
+            .whereEqualTo("is_active", true)
+            .whereEqualTo("is_deleted", false)
+            .orderBy("rating", Query.Direction.DESCENDING)
+            .limit(3)
+            .get()
+            .addOnSuccessListener { documents ->
+                sliderMovies = documents.toObjects(Movie::class.java)
+                listItems = java.util.ArrayList()
+                for (element in sliderMovies) {
+                    listItems!!.add(
+                        The_Slide_Items_Model_Class_HomePage(
+                            R.drawable.foreplay_background,
+                            element.title
+                        )
+                    )
+                }
+            }
+            .addOnFailureListener { e ->
+                Log.w("DB", "Error getting document", e)
+            }
     }
 
-    fun getDataForList () {
+    fun getDataForList() {
         arrayList.clear()
         if (current == 1) {
             // Ds phim hiện tại 6 phần tử
-            arrayList.add(Item("hello",R.drawable.foreplay_background))
-            arrayList.add(Item("hello",R.drawable.foreplay_background))
-            arrayList.add(Item("hello",R.drawable.foreplay_background))
-            arrayList.add(Item("hello",R.drawable.foreplay_background))
-            arrayList.add(Item("hello",R.drawable.foreplay_background))
-            arrayList.add(Item("hello",R.drawable.foreplay_background))
-        }
-        else {
+            val db = Firebase.firestore
+            db.collection("movie")
+                .whereEqualTo("is_active", true)
+                .whereEqualTo("is_deleted", false)
+                .orderBy("release_date", Query.Direction.DESCENDING)
+                .limit(6)
+                .get()
+                .addOnSuccessListener { documents ->
+                    airingMovies = documents.toObjects(Movie::class.java)
+                    var arrayList: ArrayList<Item> = ArrayList()
+                    for (element in airingMovies) {
+                        arrayList.add(Item(element.title, R.drawable.foreplay_background))
+                    }
+                }
+                .addOnFailureListener { e ->
+                    Log.w("DB", "Error getting document", e)
+                }
+        } else {
             // Ds phim sắp chiếu
-            arrayList.add(Item("hi",R.drawable.foreplay_background))
-            arrayList.add(Item("hi",R.drawable.foreplay_background))
-            arrayList.add(Item("hi",R.drawable.foreplay_background))
-            arrayList.add(Item("hi",R.drawable.foreplay_background))
-            arrayList.add(Item("hio",R.drawable.foreplay_background))
-            arrayList.add(Item("hi",R.drawable.foreplay_background))
+            arrayList.add(Item("hi", R.drawable.foreplay_background))
+            arrayList.add(Item("hi", R.drawable.foreplay_background))
+            arrayList.add(Item("hi", R.drawable.foreplay_background))
+            arrayList.add(Item("hi", R.drawable.foreplay_background))
+            arrayList.add(Item("hio", R.drawable.foreplay_background))
+            arrayList.add(Item("hi", R.drawable.foreplay_background))
         }
         adapter?.notifyDataSetChanged()
     }
@@ -96,7 +135,7 @@ class HomeFragment : Fragment() {
         getDataForSlider()
 
         // Test list
-        getDataForList ()
+        getDataForList()
 
         buttonCurrentFilm = view.findViewById(R.id.activity_home_page_button_current_film)
         buttonComingFilm = view.findViewById(R.id.activity_home_page_button_comming_film)
@@ -105,17 +144,18 @@ class HomeFragment : Fragment() {
             current = 0
             buttonComingFilm?.setTextColor(Color.parseColor("#FF0303"));
             buttonCurrentFilm?.setTextColor(Color.parseColor("#C8C8C8"));
-            getDataForList ()
+            getDataForList()
         }
 
         buttonCurrentFilm?.setOnClickListener {
             current = 1
             buttonComingFilm?.setTextColor(Color.parseColor("#C8C8C8"));
             buttonCurrentFilm?.setTextColor(Color.parseColor("#FF0303"));
-            getDataForList ()
+            getDataForList()
         }
 
-        val itemsPager_adapter = The_Slide_items_Pager_Adapter_HomePage(requireActivity(), listItems)
+        val itemsPager_adapter =
+            The_Slide_items_Pager_Adapter_HomePage(requireActivity(), listItems)
         page!!.adapter = itemsPager_adapter
 
         val timer = Timer()
